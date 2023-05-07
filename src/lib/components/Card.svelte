9310bc3e2c7b79c0432b4ba6a6931707eede19fb
aldairbalanzar <abalanzar7@gmail.com>
2023-05-07 01:14:10 -0400

    setup tailwind css

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import '../../app.css';
+    import penImg from "../devAssets/pen.jpeg"
+</script>
+
+<div class="">
+    <h1 class="text-3xl font-bold underline">Title</h1>
+    <img src={penImg} alt={''} class="sign-img">
+    <div class="">card description</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
 
-<script lang="ts">
+<script lang='ts'>
+import Card from "$lib/components/Card.svelte";
 </script>
+
+<Card />
+  
+<style lang="postcss">
+:global(html) {
+    background-color: theme(colors.gray.300);
+}
+</style>
