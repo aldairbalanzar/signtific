34a25472bbcd86481d2cb7c77c3de03a9ac87e09
aldairbalanzar <abalanzar7@gmail.com>
2023-05-20 16:56:29 -0400

    login to supabase works

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<main>
+    <h1 class="header">Register</h1>
+    <form action="?/login" method="POST">
+        <label for="email">Email</label>
+        <input type="text" name="email">
+        <label for="password">Password</label>
+        <input type="password" name="password">
+        <button>Login</button>
+    </form>
+</main>
