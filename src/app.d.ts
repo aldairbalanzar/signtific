3ba464b45236259c95616fcaef94fac9c7d7270e
aldairbalanzar <abalanzar7@gmail.com>
2023-10-03 04:30:16 -0400

    setup supabase client and hook

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,13 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import { Session, SupabaseClient } from "@supabase/supabase-js"
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			supabase: SupabaseClient;
+			session: Session | null;
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,25 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+  import { supabaseClient } from "$lib/supabase.js";
+	import { invalidateAll } from "$app/navigation";
+
+  export let data;
+  console.log('data: ', data);
+
+  onMount(() => {
+    const { data } = supabaseClient.auth.onAuthStateChange(() => invalidateAll());
+    const { subscription } = data;
+
+    return () => subscription.unsubscribe();
+  })
+
+</script>
+
 <nav>
   <ul>
     <li><a href="/login">Login</a></li>
     <li><a href="/register">Register</a></li>
   </ul>
 </nav>
+
 <slot/>
