ad1a9f287102aed7d91971235282ef75bfe874a9
aldairbalanzar <abalanzar7@gmail.com>
2023-10-07 01:30:51 -0400

    displaying image and video sources in Card component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import turtleVideo from '$lib/video/turtle.mp4';
+  import turtleImg from '$lib/images/turtleImg.jpg';
+</script>
+
+<div class="card-container">
+  <h1 class="card-title">Title</h1>
+  <img src={turtleImg} alt="turtle" class="card-img">
+  <video class="card-video" src={turtleVideo} >
+    <track kind="captions" />
+  </video>
+</div>
+
+<style>
+  .card-container {
+    border: 2px solid red;
+    width: 224px;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+
+  }
+  .card-title {
+    width: 100%;
+    text-align: center;
+  }
+  .card-img {
+    width: 224px;
+    object-fit: contain;
+  }
+  .card-video {
+    width: 224px;
+    object-fit: contain;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import Card from "$lib/components/Card.svelte";
+
+</script>
+
 <h1>Signtific</h1>
+<Card />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
