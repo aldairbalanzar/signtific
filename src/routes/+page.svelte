c10c91a97d8bed401d79970018e1a139a0298475
aldairbalanzar <abalanzar7@gmail.com>
2023-10-14 00:36:15 -0400

    styling for sign card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Signtific</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+  :global(body) {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -21,6 +21,62 @@ let signs: ICardSign[] = [
     video: duckVideo,
     play: false,
   },
+  {
+    id: '3',
+    title: 'turtle',
+    img: turtleImg,
+    video: turtleVideo,
+    play: false,
+  },
+  {
+    id: '4',
+    title: 'duck',
+    img: duckImg,
+    video: duckVideo,
+    play: false,
+  },
+  {
+    id: '5',
+    title: 'turtle',
+    img: turtleImg,
+    video: turtleVideo,
+    play: false,
+  },
+  {
+    id: '6',
+    title: 'duck',
+    img: duckImg,
+    video: duckVideo,
+    play: false,
+  },
+  {
+    id: '7',
+    title: 'turtle',
+    img: turtleImg,
+    video: turtleVideo,
+    play: false,
+  },
+  {
+    id: '8',
+    title: 'duck',
+    img: duckImg,
+    video: duckVideo,
+    play: false,
+  },
+  {
+    id: '9',
+    title: 'turtle',
+    img: turtleImg,
+    video: turtleVideo,
+    play: false,
+  },
+  {
+    id: '10',
+    title: 'duck',
+    img: duckImg,
+    video: duckVideo,
+    play: false,
+  },
 ];
 $: playingId = '';
 </script>
@@ -36,8 +92,9 @@ $: playingId = '';
 
 <style>
   .container {
+    width: 100%;
     display: flex;
-    justify-content: flex-start;
+    justify-content: space-evenly;
     flex-wrap: wrap;
   }
 </style>
