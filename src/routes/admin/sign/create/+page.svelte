80c36a6a123d66f641a59d5750bf165bfa94879e
aldairbalanzar <abalanzar7@gmail.com>
2023-12-09 03:37:54 -0500

    uploading image, video, and audio files to supabase

--- src/routes/admin/sign/create/+page.svelte
+++ src/routes/admin/sign/create/+page.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
+    import type { ISignUpload } from '$lib/interfaces/signUpload.js';
   export let data
-  interface ISignUpload {
-      name: string;
-      image: FileList | null;
-      video: FileList | null;
-      audio: FileList | null;
-      audioLang: string;
-  }
-
   let newSign: ISignUpload = {
     name: '',
     image: null,
@@ -15,29 +8,42 @@
     audio: null,
     audioLang: 'eng',
   }
-
   const keysOfFiles = ['image', 'video', 'audio'];
 
-
+  function getBucket(extension: string): string {
+    switch(extension) {
+        case 'jpeg':
+        case 'jpg':
+        case 'png':
+            return'sign_images'
+        case 'mp4':
+            return'sign_videos'
+        case 'mp3':
+            return'sign_audio'
+        default: 
+            return 'videogames'
+    }
+  }
   
   async function handleFileUpload() {
     const { name, image, video, audio, audioLang } = newSign;
-    if(!name || !image || !video || !audio) throw new Error('You must select an image to upload.');
+    if(!name || !image || !video || !audio) throw new Error('You must select required to upload.');
 
     for(const [key, val] of Object.entries(newSign)) {
         if(keysOfFiles.includes(key)) {
             const file = val[0];
             const fileExt = file.name.split('.').pop();
             const fileName = `${Math.random()}.${fileExt}`;
+            const bucket = getBucket(fileExt);
+
+            try {
+                const { error  } = await data.supabase.storage.from(bucket).upload(`/${fileName}`, file);
+                if(error) console.log('supabase-error: ', error);
+            } catch (error) {
+                console.log('error: ', error);
+            }
         }
     }
-    //   try {
-    //       console.log('name: ', fileName);
-    //       const { error  } = await data.supabase.storage.from('sign_images').upload(`/videogames/${fileName}`, file);
-    //       if(error) console.log('supabase-error: ', error);
-    //   } catch (error) {
-    //       console.log('error: ', error);
-    //   }
   }
   </script>
   
@@ -67,17 +73,3 @@
         <button on:click|preventDefault={handleFileUpload}>Upload</button>
     </form>
   </div>
-  <!-- <div>
-      <form method="POST">
-          <label for="file-upload"></label>
-          <input
-          required
-          accept=".jpeg, .jpg, .png"
-          bind:files
-          name="file-upload" 
-          type="file"
-          >
-  
-          <button on:click|preventDefault={handleFileUpload}>Upload</button>
-      </form>
-  </div> -->
