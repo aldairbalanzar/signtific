d34b82e55d8b57ecfd48ecb68ee91e2b85e8e9b3
aldairbalanzar <abalanzar7@gmail.com>
2023-12-26 00:02:39 -0500

    migrated hook to use supabase ssr

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,21 @@
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
-import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit'
-import { redirect } from '@sveltejs/kit';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { createServerClient } from '@supabase/ssr'
+// import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
+import { redirect, type Handle } from '@sveltejs/kit';
 
-export const handle = async ({ event, resolve }) => {
-  event.locals.supabase = createSupabaseServerClient({
-    supabaseUrl: PUBLIC_SUPABASE_URL,
-    supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
-    event,
-  })
+export const handle: Handle = async ({ event, resolve }) => {
+  event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+    cookies: {
+      get: (key) => event.cookies.get(key),
+      set: (key, value, options) => {
+        event.cookies.set(key, value, options)
+      },
+      remove: (key, options) => {
+        event.cookies.delete(key, options)
+      },
+    },
+  });
+
 
   const {data: { session }} = await event.locals.supabase.auth.getSession();
   event.locals.session = session
