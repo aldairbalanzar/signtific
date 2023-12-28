d5c4cb9fc0ad39e0c23da3e01901c2ae5d0a98d7
aldairbalanzar <abalanzar7@gmail.com>
2023-12-28 01:51:09 -0500

    shaped upload data

--- src/routes/admin/sign/create/+page.svelte
+++ src/routes/admin/sign/create/+page.svelte
@@ -1,50 +1,77 @@
 <script lang="ts">
-    import type { ISignUpload } from '$lib/interfaces/signUpload.js';
-  export let data
-  let newSign: ISignUpload = {
-    name: '',
-    image: null,
-    video: null,
-    audio: null,
-    audioLang: 'eng',
-  }
-  const keysOfFiles = ['image', 'video', 'audio'];
+import type { ISignUploadPayload, ISignUpload } from '$lib/interfaces/signUpload.js';
+export let data;
 
-  function getBucket(extension: string): string {
-    switch(extension) {
-        case 'jpeg':
-        case 'jpg':
-        case 'png':
-            return'sign_images'
-        case 'mp4':
-            return'sign_videos'
-        case 'mp3':
-            return'sign_audio'
-        default: 
-            return 'videogames'
+    let newSignPayload: ISignUploadPayload = {
+        name: '',
+        image: null,
+        video: null,
+        audio: null,
+        audioLang: 'eng',
     }
-  }
-  
-  async function handleFileUpload() {
-    const { name, image, video, audio, audioLang } = newSign;
-    if(!name || !image || !video || !audio) throw new Error('You must select required to upload.');
+    const keysOfFiles = ['image', 'video', 'audio'];
 
-    for(const [key, val] of Object.entries(newSign)) {
-        if(keysOfFiles.includes(key)) {
-            const file = val[0];
-            const fileExt = file.name.split('.').pop();
-            const fileName = `${Math.random()}.${fileExt}`;
-            const bucket = getBucket(fileExt);
-
-            try {
-                const { error  } = await data.supabase.storage.from(bucket).upload(`/${fileName}`, file);
-                if(error) console.log('supabase-error: ', error);
-            } catch (error) {
-                console.log('error: ', error);
-            }
+    function getBucket(extension: string): string {
+        switch(extension) {
+            case 'jpeg':
+            case 'jpg':
+            case 'png':
+                return'sign_images'
+            case 'mp4':
+                return'sign_videos'
+            case 'mp3':
+                return'sign_audio'
+            default: 
+                return ''
         }
     }
-  }
+
+    async function handleFileUpload() {
+        let newSign: ISignUpload = {
+            name: '',
+            image_url: '',
+            video_url: '',
+            audio_url: '',
+        }
+
+        const { name, image, video, audio, audioLang } = newSignPayload;
+        if(!name || !image || !video || !audio) throw new Error('You must fill in required fields.');
+
+        for(const [key, val] of Object.entries(newSignPayload)) {
+            if(keysOfFiles.includes(key)) {
+                const file = val[0];
+                const fileExt = file.name.split('.').pop();
+                const fileName = `${Math.random()}.${fileExt}`;
+                const bucket = getBucket(fileExt);
+
+                // try {
+                //     const { error  } = await data.supabase.storage.from(bucket).upload(`/${fileName}`, file);
+                //     if(error) console.log('supabase-error: ', error);
+                // } catch (error) {
+                //     console.log('error: ', error);
+                // }
+
+                switch(bucket) {
+                    case 'sign_images':
+                        newSign.image_url = `${bucket}/${fileName}`;
+                    case 'sign_videos':
+                        newSign.video_url = `${bucket}/${fileName}`;
+                    case 'sign_audio':
+                        newSign.audio_url = `${bucket}/${fileName}`;
+                }
+            }
+
+        }
+
+        try {
+            console.log('newSign: ', newSign);
+            const { error } = await data.supabase.from('american_signs').insert(newSign);
+            if(error) console.log('supabase-error: ', error);
+        } catch (error) {
+            console.log('error: ', error);
+        }
+
+    }
   </script>
   
   <div>
@@ -52,22 +79,22 @@
     <form method="POST">
         <div class="input-field">
             <label for="sign-name">Name: </label>
-            <input type="text" required bind:value={newSign.name}>
+            <input type="text" required bind:value={newSignPayload.name}>
         </div>
 
         <div class="input-field">
             <label for="sign-img">Image: </label>
-            <input type="file" required bind:files={newSign.image}>
+            <input type="file" required bind:files={newSignPayload.image}>
         </div>
 
         <div class="input-field">
             <label for="sign-video">Video: </label>
-            <input type="file" required bind:files={newSign.video}>
+            <input type="file" required bind:files={newSignPayload.video}>
         </div>
 
         <div class="input-field">
             <label for="sign-audio">Audio: </label>
-            <input type="file" required bind:files={newSign.audio}>
+            <input type="file" required bind:files={newSignPayload.audio}>
         </div>
 
         <button on:click|preventDefault={handleFileUpload}>Upload</button>
