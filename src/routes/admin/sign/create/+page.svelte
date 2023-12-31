c09f95608a5a6fac1662f45a5cf591b5d46c6a30
aldairbalanzar <abalanzar7@gmail.com>
2023-12-31 12:18:40 -0500

    new sign added to table and files upload into respective bucket

--- src/routes/admin/sign/create/+page.svelte
+++ src/routes/admin/sign/create/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import type { ISignUploadPayload, ISignUpload } from '$lib/interfaces/signUpload.js';
 export let data;
+const { supabase } = data;
 
     let newSignPayload: ISignUploadPayload = {
         name: '',
@@ -18,6 +19,7 @@ export let data;
             case 'png':
                 return'sign_images'
             case 'mp4':
+            case 'MP4':
                 return'sign_videos'
             case 'mp3':
                 return'sign_audio'
@@ -36,20 +38,21 @@ export let data;
 
         const { name, image, video, audio, audioLang } = newSignPayload;
         if(!name || !image || !video || !audio) throw new Error('You must fill in required fields.');
+        newSign.name = name;
 
         for(const [key, val] of Object.entries(newSignPayload)) {
             if(keysOfFiles.includes(key)) {
                 const file = val[0];
-                const fileExt = file.name.split('.').pop();
-                const fileName = `${Math.random()}.${fileExt}`;
+                const fileExt: string = file.name.split('.').pop();
+                const fileName: string = `${Math.random()}.${fileExt.toLowerCase()}`;
                 const bucket = getBucket(fileExt);
 
-                // try {
-                //     const { error  } = await data.supabase.storage.from(bucket).upload(`/${fileName}`, file);
-                //     if(error) console.log('supabase-error: ', error);
-                // } catch (error) {
-                //     console.log('error: ', error);
-                // }
+                try {
+                    const { error  } = await data.supabase.storage.from(bucket).upload(`/${fileName}`, file);
+                    if(error) console.log('supabase-error: ', error);
+                } catch (error) {
+                    console.log('error: ', error);
+                }
 
                 switch(bucket) {
                     case 'sign_images':
@@ -60,13 +63,18 @@ export let data;
                         newSign.audio_url = `${bucket}/${fileName}`;
                 }
             }
-
         }
 
         try {
-            console.log('newSign: ', newSign);
-            const { error } = await data.supabase.from('american_signs').insert(newSign);
+            console.log('new sign: ', newSign);
+            const { data, error } = await supabase.from('american_signs').insert({
+                name: newSign.name,
+                image_url: newSign.image_url,
+                video_url: newSign.video_url,
+                audio_url: newSign.audio_url,
+            });
             if(error) console.log('supabase-error: ', error);
+            console.log('data: ', data);
         } catch (error) {
             console.log('error: ', error);
         }
