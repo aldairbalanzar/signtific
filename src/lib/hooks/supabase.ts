b3df5a11ea3ea2e6f7f3e01f5365962de54dfd99
aldairbalanzar <abalanzar7@gmail.com>
2024-01-17 00:26:47 -0500

    finish migrating project to sveltekit 2.0

--- src/lib/hooks/supabase.ts
+++ src/lib/hooks/supabase.ts
@@ -2,13 +2,14 @@ import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/publi
 import type { IUser } from '$lib/interfaces/user';
 import { createServerClient } from '@supabase/ssr';
 import type { Handle } from '@sveltejs/kit';
+import type { CookieSerializeOptions } from 'cookie';
 
 export const supabase: Handle = async ({ event, resolve }) => {
   event.locals.supabase = createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
     cookies: {
       get: (key) => event.cookies.get(key),
-      set: (key, value, options) => {event.cookies.set(key, value, options)},
-      remove: (key, options) => {event.cookies.delete(key, options)},
+      set: (key, value, options) => {event.cookies.set(key, value, options as CookieSerializeOptions & { path: string })},
+      remove: (key, options) => {event.cookies.delete(key, options as CookieSerializeOptions & { path: string })},
     },
   });
 
